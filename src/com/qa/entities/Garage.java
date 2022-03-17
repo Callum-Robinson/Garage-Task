f50fd58bfa9ebd09978a71b23f9563a198c95bf9
Callum <callumjrobinson2020@gmail.com>
2022-03-17 20:08:40 +0000

    CR: Added getters and setters to Garage class

--- src/com/qa/entities/Garage.java
+++ src/com/qa/entities/Garage.java
@@ -109,7 +109,31 @@ public class Garage {
 			return 0;
 		}
 	}
+
+
 	
 	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
 	
 }
