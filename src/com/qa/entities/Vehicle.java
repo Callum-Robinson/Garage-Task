e407d604e84803e85176d9809a03efae21fdcff1
Callum <callumjrobinson2020@gmail.com>
2022-03-17 16:05:43 +0000

    CR: Create constructors, getters and setters using Eclipse

--- src/com/qa/entities/Vehicle.java
+++ src/com/qa/entities/Vehicle.java
@@ -7,5 +7,57 @@ public class Vehicle {
 	private int noOfWheels;
 	
 	
+	public Vehicle() {
+		this(0, "Unknown", 0);
+	}
+
+
+	public Vehicle(int id, String colour, int noOfWheels) {
+		super();
+		this.id = id;
+		this.colour = colour;
+		this.noOfWheels = noOfWheels;
+	}
+
+
+	
+	
+	
+	
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getColour() {
+		return colour;
+	}
+
+
+	public void setColour(String colour) {
+		this.colour = colour;
+	}
+
+
+	public int getNoOfWheels() {
+		return noOfWheels;
+	}
+
+
+	public void setNoOfWheels(int noOfWheels) {
+		this.noOfWheels = noOfWheels;
+	}
+	
+	
+	
+	
 	
 }
