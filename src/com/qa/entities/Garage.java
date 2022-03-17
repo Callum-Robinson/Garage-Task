bca513d2465f2939c3ec8444f17c2d3decb47f00
Callum <callumjrobinson2020@gmail.com>
2022-03-17 16:37:14 +0000

    CR: adding fields and constructors to garage

--- src/com/qa/entities/Garage.java
+++ src/com/qa/entities/Garage.java
@@ -3,5 +3,33 @@ package com.qa.entities;
 import java.util.ArrayList;
 
 public class Garage {
+	private static int counter = 1;
+	private int id;
+	private String name;
+	private ArrayList<Vehicle> vehicles;
+	
+	
+	public Garage() {
+		this.vehicles = new ArrayList<>();
+	}
 
+
+	public Garage(String name, ArrayList<Vehicle> vehicles) {
+		this.id = Garage.counter;
+		this.name = name;
+		this.vehicles = vehicles;
+	}
+
+
+	public Garage(int id, String name, ArrayList<Vehicle> vehicles) {
+		this.id = id;
+		this.name = name;
+		this.vehicles = vehicles;
+	}
+	
+	
+	
+	
+	
+	
 }
